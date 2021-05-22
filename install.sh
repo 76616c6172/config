672c171601326145a62ee61dba4e9c8a569dcde9
valar <76616c6172@users.noreply.github.com>
2021-05-21 20:22:03 -0700

    ADDED Safety check for calling the installer from the wrong directory

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# exit if the script is not being called from the dir it is in.
 # check if ~/.config already exists, if it does, print error and exit.
-check_dir(){
-  if test -d "$HOME/.configuration"; then
+check_dir() {
+	if test ! -e "$PWD/config/install.sh"; then
+    echo "[!][error] Installer must be called from the directory it is in! Exiting without changes."
+		exit 1
+  elif test -d "$HOME/.configuration"; then
     echo "[!][error] A directory named .configuration already exists in the home directory! Exiting without changes."
     exit
   elif test -f "$HOME/.configuration"; then
@@ -34,7 +38,7 @@ simple() {
 
   # Hiding the config folder
   echo "[*] Moving config to .configuration"
-  mv "$PWD/../config" "$HOME/.configuration"
+  mv "$PWD" "$HOME/.configuration"
 
   # Symlinking to apply the new config files
   echo "[*] Creating Symlinks.."
